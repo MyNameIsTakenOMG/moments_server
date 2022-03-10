fd071040588729333a323b8fac2bb0fa65025ddc
Sean_F <fangzhengonly@gmail.com>
2022-03-09 23:21:51 -0500

    changed to the front site domain

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -110,7 +110,7 @@ const signUpUser = async(req,res)=>{
         const token = jsonwebtoken.sign({user:newUser.username},jwt_secret,{expiresIn:'7d',algorithm:'HS256'})
         
         if(process.env.NODE_ENV!=='production') res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7})
-        else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'https://moments-frontend-netlify.netlify.app/login'})
+        else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'moments-frontend-netlify.netlify.app'})
         let newRegularUser = await User.findOne({username:newUser.username},userProjection)
         res.status(200).json({user:newRegularUser,message:'account created successfully'})
 
@@ -158,7 +158,7 @@ const signInUser = async(req,res)=>{
                 //create a JWT token and put it in the cookie ( in production, remember to setup domain for cookie)
                 const token = jsonwebtoken.sign({user:newUser.username},jwt_secret,{expiresIn:'7d',algorithm:'HS256'})
                 if(process.env.NODE_ENV!=='production') res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7})
-                else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'https://moments-frontend-netlify.netlify.app/login'})
+                else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'moments-frontend-netlify.netlify.app'})
                 let newGoogleUser = await User.findOne({username:newUser.username},userProjection)
                 res.status(200).json({user:newGoogleUser,message:'Logged in successfully'})
             }
@@ -184,7 +184,7 @@ const signInUser = async(req,res)=>{
                 //create a JWT token and put it in the cookie ( in production, remember to setup domain for cookie)
                 const token = jsonwebtoken.sign({user:isGoogleUser.username},jwt_secret,{expiresIn:'7d',algorithm:'HS256'})
                 if(process.env.NODE_ENV!=='production') res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7})
-                else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'https://moments-frontend-netlify.netlify.app/login'})
+                else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'moments-frontend-netlify.netlify.app'})
                 res.status(200).json({user:isGoogleUser,message:'Logged in successfully'})
             }
             
@@ -199,7 +199,7 @@ const signInUser = async(req,res)=>{
                     //create a JWT token and put it in the cookie ( in production, remember to setup domain for cookie)
                     const token = jsonwebtoken.sign({user:theUser.username},jwt_secret,{expiresIn:'7d',algorithm:'HS256'})
                     if(process.env.NODE_ENV!=='production') res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7})
-                    else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'https://moments-frontend-netlify.netlify.app/login'})
+                    else res.cookie('token',token,{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'moments-frontend-netlify.netlify.app'})
                     let theRegularUser = await User.findOne({username:theUser.username},userProjection)
                     res.status(200).json({user:theRegularUser,message:'Logged in successfully'})
                 }
@@ -216,7 +216,7 @@ const signInUser = async(req,res)=>{
 
 const signOutUser = (req,res)=>{
     if(process.env.NODE_ENV!=='production')res.clearCookie('token',{httpOnly:true,maxAge:60*60*24*7})
-    else res.clearCookie('token',{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'https://moments-frontend-netlify.netlify.app/login'})
+    else res.clearCookie('token',{httpOnly:true,maxAge:60*60*24*7,sameSite:'none',secure:true,domain:'moments-frontend-netlify.netlify.app'})
     res.status(200).json({message:'successfully logged out'})
 }
 
